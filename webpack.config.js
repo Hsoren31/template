c44c4ed3830622bed723178ed2cd0aa26948316f
Haley Sorensen <sorensenhaley@gmail.com>
2024-08-21 14:05:35 -0600

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { watchFile } = require('fs');
 
 module.exports = {
   mode: 'development',
   entry: {
     index: './src/index.js',
   },
-  devtool: 'inline-source-map',
+  devtool: 'eval-source-map',
+  devServer: {
+    watchFiles: ['./src/index.html']
+  },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -30,10 +34,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'index.html',
-        inject: 'head',
-        scriptLoading: 'defer',
+        template: './src/index.html'
     }),
   ],
 };
